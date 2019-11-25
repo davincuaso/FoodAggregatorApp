cd406f741383f35968d0f04b417bbe527e5b49ff
Davin Cuaso <davinsean@gmail.com>
2019-11-25 16:37:04 +0800

    added login page

--- FinalProject/app/src/main/java/com/example/finalproject/MainActivity.java
+++ FinalProject/app/src/main/java/com/example/finalproject/MainActivity.java
@@ -2,13 +2,83 @@ package com.example.finalproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.MultiplePermissionsReport;
+import com.karumi.dexter.listener.multi.BaseMultiplePermissionsListener;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+import org.androidannotations.annotations.AfterViews;
+import org.androidannotations.annotations.Click;
+import org.androidannotations.annotations.EActivity;
+import org.androidannotations.annotations.ViewById;
+
+
+@EActivity(R.layout.activity_main)
+    public class MainActivity extends AppCompatActivity{
+        @ViewById
+        TextView loginText;
+
+        @ViewById
+        EditText username;
+
+        @ViewById
+        EditText password;
+
+        @ViewById
+        Button loginButton;
+
+        @ViewById
+        Button registerButton;
+
+
+    @AfterViews
+    public void checkPermissions()
+    {
+        Dexter.withActivity(this)
+                .withPermissions(
+                        Manifest.permission.READ_EXTERNAL_STORAGE,
+                        Manifest.permission.WRITE_EXTERNAL_STORAGE,
+
+                        Manifest.permission.CAMERA
+
+                )
+
+                .withListener(new BaseMultiplePermissionsListener()
+                {
+                    public void onPermissionsChecked(MultiplePermissionsReport report)
+                    {
+                        if (report.areAllPermissionsGranted())
+                        {
+                            init();
+                        }
+                        else
+                        {
+                            Toast.makeText(getApplicationContext(), "You must provide permissions for app to run", Toast.LENGTH_LONG).show();
+                            finish();
+                        }
+                    }
+                })
+                .check();
     }
-}
+
+        public void init(){
+
+        }
+
+        @Click(R.id.loginButton)
+        public void login(){
+
+        }
+
+        @Click(R.id.registerButton)
+        public void register(){
+
+    }
+    }
+
