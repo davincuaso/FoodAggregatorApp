cd98b0f313a96b7d60621c386bd09984897585d1
Guen Reinde <adrielle.reinde@obf.ateneo.edu>
2019-12-04 16:59:58 +0800

    stall list

---
+++ FinalProject/app/src/main/java/com/example/finalproject/StallList.java
@@ -0,0 +1,49 @@
+package com.example.finalproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+import org.androidannotations.annotations.AfterViews;
+import org.androidannotations.annotations.Bean;
+import org.androidannotations.annotations.Click;
+import org.androidannotations.annotations.EActivity;
+import org.androidannotations.annotations.ViewById;
+
+import io.realm.RealmResults;
+
+@EActivity(R.layout.activity_stall_list)
+public class StallList extends AppCompatActivity {
+
+    @ViewById
+    RecyclerView stallRecyclerView;
+
+    @ViewById
+    Button backStall;
+
+    @Click(R.id.backStall)
+    public void next()
+    {
+        MainActivity_.intent(this).start();
+    }
+
+    @AfterViews
+    public void init(){
+        LinearLayoutManager mLayoutManager = new LinearLayoutManager(this);
+        mLayoutManager.setOrientation(RecyclerView.VERTICAL);
+        stallRecyclerView.setLayoutManager(mLayoutManager);
+
+        RealmResults<Stall> displaystalls = realm.findAll();
+
+    }
+
+    public void delete(Stall object)
+    {
+        realm.delete(object);
+    }
+
+}
+
