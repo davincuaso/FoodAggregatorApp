62132eae548cdc35c0369b0b338c65df32be9c69
Davin Cuaso <davinsean@gmail.com>
2019-12-06 17:19:23 +0800

    added reviews but not yet done with xml and realm

---
+++ FinalProject/app/src/main/java/com/example/finalproject/Review.java
@@ -0,0 +1,6 @@
+package com.example.finalproject;
+
+import io.realm.RealmObject;
+
+public class Review extends RealmObject {
+}
--- FinalProject/app/src/main/java/com/example/finalproject/StallManager.java
+++ FinalProject/app/src/main/java/com/example/finalproject/StallManager.java
@@ -1,63 +1,63 @@
-package com.example.finalproject;
-
-
-import android.content.Context;
-
-import org.androidannotations.annotations.EBean;
-
-import io.realm.Realm;
-import io.realm.RealmResults;
-
-@EBean
-public class StallManager {
-    Realm realm = Realm.getDefaultInstance();
-
-    Context context;
-
-    public StallManager(Context context)
-    {
-        this.context = context;
-    }
-
-    public void save(String stallname){
-        // Instantiate Plain-Old-Java-Object
-        Stall newStall = new Stall();
-
-        // Set parameters of object
-        newStall.setStallname(stallname);
-
-        // Save to realm
-        realm.beginTransaction();
-        realm.copyToRealmOrUpdate(newStall);
-        realm.commitTransaction();
-
-    }
-
-    public void delete(Stall stall)
-    {
-        realm.beginTransaction();
-
-        try{
-            user.deleteFromRealm();
-        }catch (Exception e)
-        {
-            e.printStackTrace();
-        }
-
-        realm.commitTransaction();
-    }
-
-    public RealmResults<Stall> findAll(){
-        RealmResults<Stall> stalls = realm.where(Stall.class).findAll();
-        return stalls;
-    }
-
-    public Stall checkStalls(String stallname)
-    {
-        Stall stall = realm.where(Stall.class).equalTo("stallname", stallname)
-                .findFirst();
-        return stall;
-    }
-
-}
-
+//package com.example.finalproject;
+//
+//
+//import android.content.Context;
+//
+//import org.androidannotations.annotations.EBean;
+//
+//import io.realm.Realm;
+//import io.realm.RealmResults;
+//
+//@EBean
+//public class StallManager {
+//    Realm realm = Realm.getDefaultInstance();
+//
+//    Context context;
+//
+//    public StallManager(Context context)
+//    {
+//        this.context = context;
+//    }
+//
+//    public void save(String stallname){
+//        // Instantiate Plain-Old-Java-Object
+//        Stall newStall = new Stall();
+//
+//        // Set parameters of object
+//        newStall.setStallname(stallname);
+//
+//        // Save to realm
+//        realm.beginTransaction();
+//        realm.copyToRealmOrUpdate(newStall);
+//        realm.commitTransaction();
+//
+//    }
+//
+//    public void delete(Stall stall)
+//    {
+//        realm.beginTransaction();
+//
+//        try{
+//            user.deleteFromRealm();
+//        }catch (Exception e)
+//        {
+//            e.printStackTrace();
+//        }
+//
+//        realm.commitTransaction();
+//    }
+//
+//    public RealmResults<Stall> findAll(){
+//        RealmResults<Stall> stalls = realm.where(Stall.class).findAll();
+//        return stalls;
+//    }
+//
+//    public Stall checkStalls(String stallname)
+//    {
+//        Stall stall = realm.where(Stall.class).equalTo("stallname", stallname)
+//                .findFirst();
+//        return stall;
+//    }
+//
+//}
+//
